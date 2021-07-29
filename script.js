8aa451dc4c6c2d94710e6b6774fea72f53e73cfd
Inderjeet Singh <40511783+isdhillon@users.noreply.github.com>
2021-07-29 16:21:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 //Declarations
 let drumKeys=document.querySelectorAll(".drum-keys");
 let w=document.createElement("audio");
-w.setAttribute("src","sounds/tom-1.mp3")
 let a=document.createElement("audio");
-a.setAttribute("src","sounds/tom-2.mp3")
 let s=document.createElement("audio");
-s.setAttribute("src","sounds/tom-3.mp3")
 let d=document.createElement("audio");
-d.setAttribute("src","sounds/tom-4.mp3")
 let j=document.createElement("audio");
-j.setAttribute("src","sounds/snare.mp3")
 let k=document.createElement("audio");
-k.setAttribute("src","sounds/crash.mp3")
 let l=document.createElement("audio");
-l.setAttribute("src","sounds/kick-bass.mp3")
 //event listener of click
 for(let i=0;i<drumKeys.length;i++){
     drumKeys[i].addEventListener("click",function(){
@@ -38,37 +31,45 @@ function playSound(key){
     //w 
     if(key=='w'){
         //playing sound
+        w.setAttribute("src","sounds/tom-1.mp3")
         w.play();
+        
     }
     //a
     else if(key=='a'){
+        a.setAttribute("src","sounds/tom-2.mp3")
        a.play(); 
     }
     //s
     else if(key=='s'){
+        s.setAttribute("src","sounds/tom-3.mp3")
         s.play();
     }
     //d
     else if(key=='d'){
+        d.setAttribute("src","sounds/tom-4.mp3")
         d.play();
     }
     //j
     else if(key=='j'){
+        j.setAttribute("src","sounds/snare.mp3")
         j.play();
     }
     //k
     else if(key=='k'){
+        k.setAttribute("src","sounds/crash.mp3")
         k.play();
     }
     //l
     else if(key=='l'){
+        l.setAttribute("src","sounds/kick-bass.mp3")
         l.play();
     }
 }
 //animation
 function animation(key){
     //getting the clicked button
-    let activeBtn=document.querySelector("."+key)
+    let activeBtn=document.querySelector(`.${key}`);
     //adding class
     activeBtn.classList.add("pressed");
     //removing class after 50miliseconds
